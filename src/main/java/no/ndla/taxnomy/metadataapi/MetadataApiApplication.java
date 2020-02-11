6988dd2ecfaa0d164a8010b4ffccfd88941b99bf
Jon-Eirik Pettersen <jon-eirik.pettersen@cerpus.com>
2020-02-11 15:32:51 +0100

    Initial

---
+++ src/main/java/no/ndla/taxnomy/metadataapi/MetadataApiApplication.java
@@ -0,0 +1,13 @@
+package no.ndla.taxnomy.metadataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetadataApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/ndla/taxnomy/metadataapi/MetadataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ndla.taxnomy.metadataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
